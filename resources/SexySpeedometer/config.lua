7edd1d9d92389cf5ba446dc155517337e2c9c949
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-14 13:23:28 +0330

    Fixes resources

---
+++ resources/SexySpeedometer/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.DefaultSkin = 'default_middle'
+
+-- Special cars are vehicles that will play a speed chime when going above 105 km/h.
+Config.SpecialCars    = { 'FUTO', 'BLISTA2' }
+Config.PlaySpeedChime = true
---
+++ resources/[Esx]/esx_basicneeds/server/main.lua
@@ -0,0 +1,76 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterUsableItem('bread', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('bread', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 200000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_bread'))
+end)
+
+ESX.RegisterUsableItem('water', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('water', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'thirst', 200000)
+	TriggerClientEvent('esx_basicneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_water'))
+end)
+
+TriggerEvent('es:addGroupCommand', 'heal', 'admin', function(source, args, user)
+	-- heal another player - don't heal source
+	if args[1] then
+		local target = tonumber(args[1])
+		
+		-- is the argument a number?
+		if target ~= nil then
+			
+			-- is the number a valid player?
+			if GetPlayerName(target) then
+				print('esx_basicneeds: ' .. GetPlayerName(source) .. ' is healing a player!')
+				TriggerClientEvent('esx_basicneeds:healPlayer', target)
+				TriggerClientEvent('chatMessage', target, "HEAL", {223, 66, 244}, "You have been healed!")
+			else
+				TriggerClientEvent('chatMessage', source, "HEAL", {255, 0, 0}, "Player not found!")
+			end
+		else
+			TriggerClientEvent('chatMessage', source, "HEAL", {255, 0, 0}, "Incorrect syntax! You must provide a valid player ID")
+		end
+	else
+		-- heal source
+		print('esx_basicneeds: ' .. GetPlayerName(source) .. ' is healing!')
+		TriggerClientEvent('esx_basicneeds:healPlayer', source)
+	end
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "HEAL", {255, 0, 0}, "Insufficient Permissions.")
+end, {help = "Heal a player, or yourself - restores thirst, hunger and health."})
+
+ESX.RegisterUsableItem('icetea', function(source)
+
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('icetea', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'thirst', 300000)
+    TriggerClientEvent('esx_basicneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_icetea'))
+
+end)
+
+ESX.RegisterUsableItem('mixapero', function(source)
+    
+        local xPlayer = ESX.GetPlayerFromId(source)
+    
+        xPlayer.removeInventoryItem('mixapero', 1)
+    
+        TriggerClientEvent('esx_status:add', source, 'hunger', 100000)
+        TriggerClientEvent('esx_status:remove', source, 'thirst', 50000)
+        TriggerClientEvent('esx_basicneeds:onEat', source)
+        TriggerClientEvent('esx:showNotification', source, _U('used_mixapero'))
+    
+end)
