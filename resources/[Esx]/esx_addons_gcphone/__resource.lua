6a8c04919765030a27e1d7ee370363a9b067cb1f
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-16 13:01:17 +0330

    Changes phone translates

--- resources/[Esx]/esx_addons_gcphone/__resource.lua
+++ resources/[Esx]/esx_addons_gcphone/__resource.lua
@@ -7,16 +7,3 @@ server_script {
 	"server.lua"
 }
 
-
--- {
--- 	"display": "Police",
--- 	"subMenu": [
--- 		{
--- 			"title": "Envoyer un message",
--- 			"eventName": "esx_addons_gcphone:call",
--- 			"type": {
--- 				"number": "police"
--- 			}
--- 		}
--- 	]
--- }
