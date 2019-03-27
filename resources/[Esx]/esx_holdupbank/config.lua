f2ad778b3414f2cf56577e14863c17eded6f032a
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-27 05:47:25 +0430

    Changes prizes

--- resources/[Esx]/esx_holdupbank/config.lua
+++ resources/[Esx]/esx_holdupbank/config.lua
@@ -11,13 +11,13 @@ Banks = {
 	},
 	["fleeca2"] = {
 		position = { ['x'] = -2957.6674804688, ['y'] = 481.45776367188, ['z'] = 15.697026252747 },
-		reward = math.random(15000,25000),
+		reward = math.random(15000,150000),
 		nameofbank = "Fleeca Bank (Highway)",
 		lastrobbed = 0
 	},
 	["blainecounty"] = {
 		position = { ['x'] = -107.06505584717, ['y'] = 6474.8012695313, ['z'] = 31.62670135498 },
-		reward = math.random(15000,25000),
+		reward = math.random(15000,150000),
 		nameofbank = "Blaine County Savings",
 		lastrobbed = 0
 	},
