481e1193c9f1421781b5ed1d42852f2b99f6cdb5
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-28 23:52:20 +0430

    Change jailed person skin

--- resources/[Esx]/esx_jailwork/client/utils.lua
+++ resources/[Esx]/esx_jailwork/client/utils.lua
@@ -52,21 +52,25 @@ function Cutscene()
 	TriggerEvent('skinchanger:getSkin', function(skin)
 		if GetHashKey(GetEntityModel(PlayerPedId())) == Male then
 			local clothesSkin = {
-				['tshirt_1'] = 20, ['tshirt_2'] = 15,
-				['torso_1'] = 33, ['torso_2'] = 0,
-				['arms'] = 0,
-				['pants_1'] = 7, ['pants_2'] = 0,
-				['shoes_1'] = 34, ['shoes_2'] = 0,
+				['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+				['torso_1']  = 146, ['torso_2']  = 0,
+				['decals_1'] = 0,   ['decals_2'] = 0,
+				['arms']     = 0,   ['pants_1']  = 3,
+				['pants_2']  = 7,   ['shoes_1']  = 12,
+				['shoes_2']  = 12,  ['chain_1']  = 50,
+				['chain_2']  = 0
 			}
 			TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
 
 		else
 			local clothesSkin = {
-				['tshirt_1'] = 15, ['tshirt_2'] = 0,
-				['torso_1'] = 2, ['torso_2'] = 6,
-				['arms'] = 2,
-				['pants_1'] = 2, ['pants_2'] = 0,
-				['shoes_1'] = 35, ['shoes_2'] = 0,
+				['tshirt_1'] = 3,   ['tshirt_2'] = 0,
+				['torso_1']  = 38,  ['torso_2']  = 3,
+				['decals_1'] = 0,   ['decals_2'] = 0,
+				['arms']     = 2,   ['pants_1']  = 3,
+				['pants_2']  = 15,  ['shoes_1']  = 66,
+				['shoes_2']  = 5,   ['chain_1']  = 0,
+				['chain_2']  = 2
 			}
 			TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
 		end
