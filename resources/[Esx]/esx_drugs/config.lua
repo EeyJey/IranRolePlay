7bcd92526480d7bfde959cd9902934ea4ad32b3f
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-28 22:36:22 +0430

    Changes z size of marker

--- resources/[Esx]/esx_drugs/config.lua
+++ resources/[Esx]/esx_drugs/config.lua
@@ -1,7 +1,7 @@
 Config              = {}
 Config.MarkerType   = 1
 Config.DrawDistance = 100.0
-Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 2.0}
 Config.MarkerColor  = {r = 100, g = 204, b = 100}
 Config.ShowBlips   = true  --markers visible on the map? (false to hide the markers on the map)
 
