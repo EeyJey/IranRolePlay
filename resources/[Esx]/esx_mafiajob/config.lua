9d3a00c74ca2961be062af57154fc5cb3e2bc726
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-29 23:58:47 +0430

    Adds new mafia

---
+++ resources/[Esx]/esx_mafiajob/config.lua
@@ -0,0 +1,95 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MarkerType                 = 1
+Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
+Config.MarkerColor                = { r = 50, g = 50, b = 204 }
+Config.EnablePlayerManagement     = true
+Config.EnableArmoryManagement     = true
+Config.EnableESXIdentity          = true -- only turn this on if you are using esx_identity
+Config.EnableNonFreemodePeds      = false -- turn this on if you want custom peds
+Config.EnableSocietyOwnedVehicles = false
+Config.EnableLicenses             = false
+Config.MaxInService               = -1
+Config.Locale                     = 'en'
+
+Config.MafiaStations = {
+
+  Mafia = {
+
+    Blip = {
+--      Pos     = { x = 425.130, y = -979.558, z = 30.711 },
+      Sprite  = 60,
+      Display = 4,
+      Scale   = 1.2,
+      Colour  = 29,
+    },
+
+    AuthorizedWeapons = {
+      { name = 'WEAPON_NIGHTSTICK',       price = 9000 },
+      { name = 'WEAPON_COMBATPISTOL',     price = 30000 },
+      { name = 'WEAPON_ASSAULTSMG',       price = 1125000 },
+      { name = 'WEAPON_ASSAULTRIFLE',     price = 1500000 },
+      { name = 'WEAPON_PUMPSHOTGUN',      price = 600000 },
+      { name = 'WEAPON_STUNGUN',          price = 50000 },
+      { name = 'WEAPON_FLASHLIGHT',       price = 800 },
+      { name = 'WEAPON_FIREEXTINGUISHER', price = 1200 },
+      { name = 'WEAPON_FLAREGUN',         price = 6000 },
+      { name = 'GADGET_PARACHUTE',        price = 3000 },
+      { name = 'WEAPON_STICKYBOMB',       price = 200000 },
+      { name = 'WEAPON_SNIPERRIFLE',      price = 2200000 },
+      { name = 'WEAPON_FIREWORK',         price = 30000 },
+      { name = 'WEAPON_GRENADE',          price = 180000 },
+      { name = 'WEAPON_BZGAS',            price = 120000 },
+      { name = 'WEAPON_SMOKEGRENADE',     price = 100000 },
+      { name = 'WEAPON_APPISTOL',         price = 70000 },
+      { name = 'WEAPON_CARBINERIFLE',     price = 1100000 },
+      { name = 'WEAPON_HEAVYSNIPER',      price = 2000000 },
+      { name = 'WEAPON_MINIGUN',          price = 700000 },
+      { name = 'WEAPON_RAILGUN',          price = 2500000 },
+    },
+
+	  AuthorizedVehicles = {
+		  { name = 'schafter3',  label = 'Véhicule Civil' },
+		  { name = 'sandking',   label = '4X4' },
+		  { name = 'mule3',      label = 'Camion de Transport' },
+		  { name = 'guardian',   label = 'Grand 4x4' },
+		  { name = 'burrito3',   label = 'Fourgonnette' },
+		  { name = 'mesa',       label = 'Tout-Terrain' },
+	  },
+
+    Cloakrooms = {
+      { x = 9.283, y = 528.914, z = 169.635 },
+    },
+
+    Armories = {
+      { x = 1.550, y = 527.397, z = 169.617 },
+    },
+
+    Vehicles = {
+      {
+        Spawner    = { x = 13.40, y = 549.1, z = 175.187 },
+        SpawnPoint = { x = 8.237, y = 556.963, z = 175.266 },
+        Heading    = 90.0,
+      }
+    },
+
+    Helicopters = {
+      {
+        Spawner    = { x = 20.312, y = 535.667, z = 173.627 },
+        SpawnPoint = { x = 3.40, y = 525.56, z = 177.919 },
+        Heading    = 0.0,
+      }
+    },
+
+    VehicleDeleters = {
+      { x = 22.74, y = 545.9, z = 175.027 },
+      { x = 21.35, y = 543.3, z = 175.027 },
+    },
+
+    BossActions = {
+      { x = 4.113, y = 526.897, z = 173.628 }
+    },
+
+  },
+
+}
