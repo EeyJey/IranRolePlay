5fb354788c3d7a323f843f6d209162ecffdf77b6
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-29 22:46:40 +0430

    Makes maintenence work

--- resources/[Esx]/esx_maintenance/server/main.lua
+++ resources/[Esx]/esx_maintenance/server/main.lua
@@ -16,18 +16,25 @@ end
 
 AddEventHandler('playerConnecting', function(peerName, setKickReason)
     local numIds = GetNumPlayerIdentifiers(source)
+	local isAllowed = false
 	for i,allowedID in ipairs(cfg.AllowedIDs) do
-        for i = 0, numIds-3 do
-			print("1", getPlayerFromId(source))
-			print("2", allowedID)
-            if cfg.isOnMaintenance and getPlayerFromId(source) ~= allowedID then
-                print('-----------------ESX MAINTENANCE SYSTEM-----------')
-                print('Dropping Connection for: [' .. peerName ..']      ')
-                print('-----------------ESX MAINTENANCE SYSTEM-----------')
-                
-				setKickReason('Server dar daste sakht mibashad, Badan moraje e konid!')
-				CancelEvent()
-			end
+		print("1", getPlayerFromId(source))
+		print("2", allowedID)
+		if cfg.isOnMaintenance and getPlayerFromId(source) == allowedID then
+			isAllowed = true
+			break
 		end
     end
+	if not isAllowed then
+		print('^1-----------------ESX MAINTENANCE SYSTEM-----------')
+		print('Dropping Connection for: [' .. peerName ..']      ')
+		print('-----------------ESX MAINTENANCE SYSTEM-----------^7')
+		
+		setKickReason('Server dar daste sakht mibashad, Badan moraje e konid!')
+		CancelEvent()
+	else
+		print('^2-----------------ESX MAINTENANCE SYSTEM-----------')
+		print('Dropping Connection for: [' .. peerName ..']      ')
+		print('-----------------ESX MAINTENANCE SYSTEM-----------^7')
+	end
 end)
