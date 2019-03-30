84dcf224b58cf3062a5ca680a3b22ab319f04102
MamadLord <Mohammadmehdish@gmail.com>
2019-03-31 00:50:14 +0430

    DMV fully Persianized

--- resources/[Esx]/esx_dmvschool/html/questions.js
+++ resources/[Esx]/esx_dmvschool/html/questions.js
@@ -1,91 +1,91 @@
 var tableauQuestion = [
 	{
-		question: "If you're going 80 km/h, and you're approaching a residential area you must:",
-		propositionA: "You accelerate",
-		propositionB: "You keep your speed, if you do not pass other vehicles",
-		propositionC: "You slow down",
-		propositionD: "You keep your speed",
-		reponse: "C"
-	},
-
-	{
-		question: "If you're turning right at a traffic light, but see a pedestrian crossing what do you do:",
-		propositionA: "You pass the pedestrian",
-		propositionB: "You check that there is no other vehicles around",
-		propositionC: "You wait until the pedestrian has crossed",
-		propositionD: "You shoot the pedestrian and continue to drive",
-		reponse: "C"
-	},
-
-	{
-		question: "Without any prior indication, the speed in a residential area is: __ km/h",
-		propositionA: "30 km/h",
-		propositionB: "50 km/h",
-		propositionC: "40 km/h",
-		propositionD: "60 km/h",
+		question: "دو خط ممتد در کنار هم به معنای ……  :",
+		propositionA: "زیبایی بیشتر",
+		propositionB: "تاکید بیشتر برای حرکت نکردن روی آن ها",
+		propositionC: "گردش روی این خطوط انجام میگیرد",
+		propositionD: "فقط سبقت روی این خطوط انجام میگیرد",
 		reponse: "B"
 	},
 
 	{
-		question: "Before every lane change you must:",
-		propositionA: "Check your mirrors",
-		propositionB: "Check your blind spots",
-		propositionC: "Signal your intentions",
-		propositionD: "All of the above",
-		reponse: "D"
+		question: "حداکثر سرعت در آزادراه ها:",
+		propositionA: "95 کیلومتر",
+		propositionB: "120 کیلومتر",
+		propositionC: "125 کیلومتر",
+		propositionD: "110 کیلومتر",
+		reponse: "B"
 	},
 
 	{
-		question: "What blood alcohol level is classified as driving while intoxicated?",
-		propositionA: "0.05%",
-		propositionB: "0.18%",
-		propositionC: "0.08%",
-		propositionD: "0.06%",
-		reponse: "C"
-	},
-
-	{
-		question: "When can you continue to drive at a traffic light?",
-		propositionA: "When it is green",
-		propositionB: "When there is nobody in the intersection",
-		propositionC: "You are in a school zone",
-		propositionD: "When it is green and / or red and you're turning right",
-		reponse: "D"
-	},
-
-	{
-		question: "A pedestrian has a do not cross signal, what do you do?",
-		propositionA: "You let them pass",
-		propositionB: "You observe before continuing",
-		propositionC: "You wave to tell them to cross",
-		propositionD: "You continue because your traffic light is green",
-		reponse: "D"
-	},
-
-	{
-		question: "What is allowed when passing another vehicle",
-		propositionA: "You follow it closely to pass it faster",
-		propositionB: "You pass it without leaving the roadway",
-		propositionC: "You drive on the opposite side of the road to pass",
-		propositionD: "You exceed the speed limit to pass them",
-		reponse: "C"
-	},
-
-	{
-		question: "You are driving on a highway which indicates a maximum speed of 120 km/h. But most trafficers drive at 125 km/h, so you should not drive faster than:",
-		propositionA: "120 km/h",
-		propositionB: "125 km/h",
-		propositionC: "130 km/h",
-		propositionD: "110 km/h",
+		question: "در خیابان یکطرفه باید درکدام سمت اتومبیل را پارک کرد؟",
+		propositionA: "راست",
+		propositionB: "چپ",
+		propositionC: "هر دو طرف آزاد است",
+		propositionD: "پارک کردن در خیابان های یکطرفه ممنوع است",
 		reponse: "A"
 	},
 
 	{
-		question: "When you are overtaken by another vehicle it is important NOT to:",
-		propositionA: "Slow Down",
-		propositionB: "Check your mirrors",
-		propositionC: "Watch other drivers",
-		propositionD: "Increase your speed",
+		question: "چه مواقعی راننده نسبت به اتومبیل کنترل کمتری دارد؟",
+		propositionA: "سربالایی",
+		propositionB: "در پیچ ها",
+		propositionC: "حرکت با دنده خلاص",
+		propositionD: "هیچ کدام",
+		reponse: "C"
+	},
+
+	{
+		question: "در حریم تابلوی ایستادن ممنوع:",
+		propositionA: "فقط برای سوار و پیاده کردن مسافر، توقف بلامانع است",
+		propositionB: "برای یک لحظه هم توقف نمی کنید",
+		propositionC: "به هیچ وجه نمی توان پارک کرد ولی می توان توقف کرد",
+		propositionD: "بستگی به موقعیت دارد",
+		reponse: "B"
+	},
+
+	{
+		question: "تابلو به شکل پنج گوشه با نقطه راس فوقانی برای … می باشد.",
+		propositionA: "ایست",
+		propositionB: "علایم هشدار برای مدرسه و دبیرستان",
+		propositionC: "معمولا برای علایم راهنما",
+		propositionD: "برای خطرات ناشی از تعمیر و نگهداری",
+		reponse: "B"
+	},
+
+	{
+		question: " چراغ توقف پیاده رو روشن است، شما چه باید بکنید؟",
+		propositionA: "اجازه ی عبور میدهید",
+		propositionB: "قبل حرکت مشاهده می کنید.",
+		propositionC: "با دست اشاره میکنید که پیاده ها عبور کنند",
+		propositionD: "شما عبور میکنید چون چراغ راهنمایی سبز است",
 		reponse: "D"
+	},
+
+	{
+		question: "هنگام سبقت از ماشین های دیگر چه کاری باید انجام دهید؟",
+		propositionA: "نزدیک به آن حرکت میکنید تا سبقت بگیرید",
+		propositionB: "بدون عبور از خط وسط از آن عبور میکنید",
+		propositionC: "در قسمت مخالف جاده قرار میگیرید تا از آن عبور کنید",
+		propositionD: "سرعت خود را افزایش میدید تا از آن عبور کنید",
+		reponse: "C"
+	},
+
+	{
+		question: "حداکثر سرعت در جاده های اصلی خارج از شهر:",
+		propositionA: "90 کیلومتر",
+		propositionB: "95 کیلومتر",
+		propositionC: "75 کیلومتر",
+		propositionD: "110 کیلومتر",
+		reponse: "A"
+	},
+
+	{
+		question: "اگر به هنگام رانندگی در آزادراه تایر خودرو پنچر شود چه باید کرد؟",
+		propositionA: "باید در همان محل توقف کرد",
+		propositionB: "باید در خط اضطراری توقف کرد",
+		propositionC: "می توان بین خطوط عبوری آزادراه توقف کرد",
+		propositionD: "موارد الف و ج صحیح است",
+		reponse: "B"
 	},
 ]
