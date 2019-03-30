16cbe366fe43e5d63fb7dbb1ea15eaa501bb1b45
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-30 19:18:08 +0430

    Changes to Mysql async

--- resources/[Vidat]/question_rp/server.lua
+++ resources/[Vidat]/question_rp/server.lua
@@ -9,6 +9,10 @@ end)
 RegisterServerEvent("antirpquestion:success")
 AddEventHandler("antirpquestion:success", function()
 	TriggerEvent("es:getPlayerFromId", source, function(player)
+		MySQL.Async.execute(
+			"UPDATE users SET question_rp='made' WHERE identifier = '@username'", { ['@username'] = player.identifier},
+				function ()
+			end)
 		MySQL:executeQuery("UPDATE users SET question_rp='made' WHERE identifier = '@username'", { ['@username'] = player.identifier})
 	end)
 end)
@@ -23,6 +27,20 @@ AddEventHandler("antirpquestion:didQuestion", function()
         if (questionMade == "false") then
             TriggerClientEvent('antirpquestion:notMade',source)
         end
+		
+	MySQL.Async.fetchAll(
+    "SELECT * FROM users WHERE identifier = '@username'",{['@username'] = player.identifier},
+    function (data)
+		BanListHistory = {}
+
+		for i=1, #data, 1 do
+			local result = data[i].question_rp
+			local questionMade = result[1].question_rp
+			if (questionMade == "false") then
+				TriggerClientEvent('antirpquestion:notMade',source)
+			end
+		end
+	)	
     end)
 end)
 
