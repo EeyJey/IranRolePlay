1566d48360e86403643558ee156404fa3a221fa0
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-30 19:28:16 +0430

    Removes old sql system

--- resources/[Vidat]/question_rp/server.lua
+++ resources/[Vidat]/question_rp/server.lua
@@ -21,13 +21,6 @@ end)
 RegisterServerEvent("antirpquestion:didQuestion")
 AddEventHandler("antirpquestion:didQuestion", function()
     TriggerEvent("es:getPlayerFromId", source, function(player)
-		local executed_query = MySQL:executeQuery("SELECT * FROM users WHERE identifier = '@username'",{['@username'] = player.identifier})
-		local result = MySQL:getResults(executed_query, {'question_rp'}, "question_rp")
-        local questionMade = result[1].question_rp
-        if (questionMade == "false") then
-            TriggerClientEvent('antirpquestion:notMade',source)
-        end
-		
 		MySQL.Async.fetchAll(
 		"SELECT * FROM users WHERE identifier = '@username'",{['@username'] = player.identifier},
 		function (data)
