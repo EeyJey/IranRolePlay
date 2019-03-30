a481b87514a9be5a084e29fd5c649267d397493e
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-03-30 21:42:31 +0430

    Changes to esx system

--- resources/[Vidat]/question_rp/server.lua
+++ resources/[Vidat]/question_rp/server.lua
@@ -1,6 +1,10 @@
 -- require "resources/essentialmode/lib/MySQL"
 
 --[[ ***** EVENTS GUI ***** ]]
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 RegisterServerEvent("antirpquestion:kick")
 AddEventHandler("antirpquestion:kick", function()
 	DropPlayer(source, "Be soalat natoonestid javab bedid.")
@@ -20,20 +24,21 @@ end)
 --[[ ***** SPAWN ***** ]]
 RegisterServerEvent("antirpquestion:didQuestion")
 AddEventHandler("antirpquestion:didQuestion", function()
-    TriggerEvent("es:getPlayerFromId", source, function(player)
-		MySQL.Async.fetchAll(
-		"SELECT * FROM users WHERE identifier = '@username'",{['@username'] = player.identifier},
-		function (data)
-			print("here",data[1])
-			for i=1, #data, 1 do
-				local result = data[i].question_rp
-				print("result", result)
-				local questionMade = result[1].question_rp
-				if (questionMade == "false") then
-					TriggerClientEvent('antirpquestion:notMade',source)
-				end
-			end	
-		end)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	
+	MySQL.Async.fetchAll(
+	"SELECT * FROM users WHERE identifier = '@username'",{['@username'] = xPlayer.identifier},
+	function (data)
+		print("here",data[1])
+		for i=1, #data, 1 do
+			local result = data[i].question_rp
+			print("result", result)
+			local questionMade = result[1].question_rp
+			if (questionMade == "false") then
+				TriggerClientEvent('antirpquestion:notMade',source)
+			end
+		end	
 	end)
+
 end)
 
