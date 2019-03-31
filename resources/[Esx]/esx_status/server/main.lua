cce8dedaab4136770ffddf6c42e41e86b6ffbd44
Ali EbrahimiNejad <a.ebrahiminejad@aut.ac.ir>
2019-04-01 01:26:37 +0430

    Revert to old esx_status

--- resources/[Esx]/esx_status/server/main.lua
+++ resources/[Esx]/esx_status/server/main.lua
@@ -1,4 +1,5 @@
-ESX = nil
+ESX                    = nil
+local RegisteredStatus = {}
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
@@ -6,30 +7,43 @@ AddEventHandler('esx:playerLoaded', function(source)
 	local _source        = source
 	local xPlayer        = ESX.GetPlayerFromId(_source)
 
-	MySQL.Async.fetchAll('SELECT status FROM users WHERE identifier = @identifier', {
+	MySQL.Async.fetchAll(
+		'SELECT * FROM users WHERE identifier = @identifier',
+		{
 			['@identifier'] = xPlayer.identifier
-	}, function(result)
-		local data = {}
+		},
+		function(result)
 
-		if result[1].status ~= nil then
-			data = json.decode(result[1].status)
+			local data = {}
+
+			if result[1].status ~= nil then
+				data = json.decode(result[1].status)
+			end
+
+			xPlayer.set('status', data)
+
+			TriggerClientEvent('esx_status:load', _source, data)
+
 		end
+	)
 
-		xPlayer.set('status', data)
-		TriggerClientEvent('esx_status:load', _source, data)
-	end)
 end)
 
 AddEventHandler('esx:playerDropped', function(source)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
+
 	local data   = {}
 	local status = xPlayer.get('status')
 
-	MySQL.Async.execute('UPDATE users SET status = @status WHERE identifier = @identifier', {
-		['@status']     = json.encode(status),
-		['@identifier'] = xPlayer.identifier
-	})
+	MySQL.Async.execute(
+		'UPDATE users SET status = @status WHERE identifier = @identifier',
+		{
+			['@status']     = json.encode(status),
+			['@identifier'] = xPlayer.identifier
+		}
+	)
+
 end)
 
 AddEventHandler('esx_status:getStatus', function(playerId, statusName, cb)
@@ -42,29 +56,36 @@ AddEventHandler('esx_status:getStatus', function(playerId, statusName, cb)
 			break
 		end
 	end
+
 end)
 
 RegisterServerEvent('esx_status:update')
 AddEventHandler('esx_status:update', function(status)
-	local xPlayer = ESX.GetPlayerFromId(source)
-
-	if xPlayer then
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if xPlayer ~= nil then
 		xPlayer.set('status', status)
 	end
 end)
 
 function SaveData()
 	local xPlayers = ESX.GetPlayers()
-
+	
 	for i=1, #xPlayers, 1 do
+
 		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
 		local data    = {}
 		local status  = xPlayer.get('status')
 
-		MySQL.Async.execute('UPDATE users SET status = @status WHERE identifier = @identifier', {
-			['@status']     = json.encode(status),
-			['@identifier'] = xPlayer.identifier
-		})
+		MySQL.Async.execute(
+			'UPDATE users SET status = @status WHERE identifier = @identifier',
+		 	{
+		 		['@status']     = json.encode(status),
+		 		['@identifier'] = xPlayer.identifier
+		 	}
+		)
+	
 	end
 
 	SetTimeout(10 * 60 * 1000, SaveData)
