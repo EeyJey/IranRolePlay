7b73feb00face9d9061964166ea6affd0145fc5b
MamadLord <Mohammadmehdish@gmail.com>
2019-04-22 03:36:49 +0430

    bank money updated

--- resources/[Esx]/esx_holdupbank/config.lua
+++ resources/[Esx]/esx_holdupbank/config.lua
@@ -5,25 +5,25 @@ Config.NumberOfCopsRequired = 5
 Banks = {
 	["fleeca"] = {
 		position = { ['x'] = 147.04908752441, ['y'] = -1044.9448242188, ['z'] = 29.36802482605 },
-		reward = math.random(300000,500000),
+		reward = math.random(500000,700000),
 		nameofbank = "Nordea",
 		lastrobbed = 0
 	},
 	["fleeca2"] = {
 		position = { ['x'] = -2957.6674804688, ['y'] = 481.45776367188, ['z'] = 15.697026252747 },
-		reward = math.random(300000,500000),
+		reward = math.random(500000,700000),
 		nameofbank = "Fleeca Bank (Highway)",
 		lastrobbed = 0
 	},
 	["blainecounty"] = {
 		position = { ['x'] = -107.06505584717, ['y'] = 6474.8012695313, ['z'] = 31.62670135498 },
-		reward = math.random(300000,500000),
+		reward = math.random(500000,700000),
 		nameofbank = "Blaine County Savings",
 		lastrobbed = 0
 	},
 	["PrincipalBank"] = {
 		position = { ['x'] = 255.001098632813, ['y'] = 225.855895996094, ['z'] = 101.005694274902 },
-		reward = math.random(700000,1000000),
+		reward = math.random(1000000,1500000),
 		nameofbank = "Swedbank",
 		lastrobbed = 0
 	}
