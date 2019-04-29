1f62858efbd4f3a6167868c754f6b560d448abec
MamadLord <Mohammadmehdish@gmail.com>
2019-04-29 23:22:54 +0430

    discord loop check

--- resources/[MaMad]/spideything/SERVER/Server.lua
+++ resources/[MaMad]/spideything/SERVER/Server.lua
@@ -169,6 +169,7 @@ AddEventHandler('DiscordBot:ToDiscord', function(WebHook, Name, Message, Image, 
 					for i, Line in ipairs(SteamProfileSplitted) do
 						if Line:find('<avatarFull>') then
 							Image = Line:gsub('	<avatarFull><!%[CDATA%[', ''):gsub(']]></avatarFull>', '')
+							print('In loop of discord '.. Message)
 							return PerformHttpRequest(WebHook, function(Error, Content, Head) end, 'POST', json.encode({username = Name, content = Message, avatar_url = Image, tts = TTS}), {['Content-Type'] = 'application/json'})
 						end
 					end
