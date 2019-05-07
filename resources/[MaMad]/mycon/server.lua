cb5956707c9659718e2a98dd2df253c73c8d0c8e
MamadLord <Mohammadmehdish@gmail.com>
2019-05-07 07:07:56 +0430

    further test on mycon

--- resources/[MaMad]/mycon/server.lua
+++ resources/[MaMad]/mycon/server.lua
@@ -1,16 +1,15 @@
 
-AddEventHandler('rconCommand', function(commandName, args)
-    if commandName == 'mystatus' then
-        for netid, data in pairs(names) do
-            local guid = GetPlayerIdentifiers(netid)
+RegisterCommand('mystatus', function(args)
+    print(args[0])
+    -- for netid, data in pairs(names) do
+    --     local guid = GetPlayerIdentifiers(netid)
 
-            if guid and guid[1] and data then
-                local ping = GetPlayerPing(netid)
+    --     if guid and guid[1] and data then
+    --         local ping = GetPlayerPing(netid)
 
-                RconPrint(netid .. '\t' .. guid[1] .. '\t' .. data.name .. '\t' .. GetPlayerEP(netid) .. '\t' .. ping .. "\n")
-            end
-        end
+    --         RconPrint(netid .. '\t' .. guid[1] .. '\t' .. data.name .. '\t' .. GetPlayerEP(netid) .. '\t' .. ping .. "\n")
+    --     end
+    -- end
 
-        CancelEvent()
-    end
+    -- CancelEvent()
 end)
