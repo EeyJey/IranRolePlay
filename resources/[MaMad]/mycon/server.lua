854b468ac5b472962df2410bfc971180517dc24b
MamadLord <Mohammadmehdish@gmail.com>
2019-05-07 08:00:11 +0430

    WTF file name?! :|

--- resources/[MaMad]/mycon/server.lua
+++ resources/[MaMad]/mycon/server.lua
@@ -13,28 +13,3 @@ RegisterCommand('mystatus', function(source, args, raw)
 
     -- CancelEvent()
 end, true)
-
-
-
-RegisterCommand('myrole', function(source, args, raw)
-	local player = tonumber(args[1])
-	local role = table.concat(args, " ", 2)
-	if args[1] then
-		if (player and GetPlayerName(player)) then
-			if args[2] then
-				TriggerEvent("es:getPlayerFromId", player, function(user)
-					user.giveRole(role)
-					TriggerClientEvent('chat:addMessage', user.get('source'), {
-						args = {"^1SYSTEM", "You've been given a role: ^2" .. role}
-					})
-				end)
-			else
-				RconPrint("Usage: giverole [user-id] [role]\n")
-			end
-		else
-			RconPrint("Player not ingame\n")
-		end
-	else
-		RconPrint("Usage: giverole [user-id] [role]\n")
-	end
-end, true)
