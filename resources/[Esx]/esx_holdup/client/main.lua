f8cd381b5c3eb5e36e4fe913e2797821559e7ae4
MamadLord <Mohammadmehdish@gmail.com>
2019-05-08 08:19:49 +0430

    WTF is going on?!

--- resources/[Esx]/esx_holdup/client/main.lua
+++ resources/[Esx]/esx_holdup/client/main.lua
@@ -91,7 +91,7 @@ AddEventHandler('esx_holdup:starttimer', function()
 		while true do
 			Citizen.Wait(0)
 			if holdingup then
-				print(timer)
+				print(_U('robbery_timer', timer))
 				drawTxt(0.66, 1.74, 1.0,1.0,0.4, _U('robbery_timer', timer), 255, 255, 255, 255)
 			else
 				Citizen.Wait(1000)
