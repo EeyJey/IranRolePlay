970427040c603223752c0b08209be4e23e564128
amirhossein <amir_hossein8731@yahoo.com>
2019-05-12 01:43:14 +0430

    fix

--- resources/[aj]/irrp_familiesprop/config.lua
+++ resources/[aj]/irrp_familiesprop/config.lua
@@ -43,38 +43,37 @@ Config.families = {
         { x = 150.89761352539, y = -2204.248046875, z = 3.688024520874 },
       }
     },
-  },
-  'LA-FUENTE' = {
-    AuthorizedWeapons = {
-      { name = 'WEAPON_PISTOL',      price = 10000 },
-    },
-    AuthorizedVehicles = {
-      { name = 'entity2',  label = 'entity' },
-      { name = 'prototipo',    label = 'prototipo' },
-      { name = 'btype3',   label = 'btype' },
-      { name = 'ztype',      label = 'ztype' },
-      { name = 'dubsta3',      label = 'dubsta' },
-    },
-    Cloakrooms = {
-      { x = 1407.1309, y = 1153.2783, z = 114.4430},
-    },
-    Armories = {
-      { x = 1406.1706, y = 1156.8936, z = 114.4430},
-    },
-    Vehicles = {
-      {
-        Spawner    = { x = 1402.3652, y = 1117.6643, z = 114.8376 },
-        SpawnPoint = { x = 1392.2114, y = 1118.1031, z = 114.8369 },
-        Heading    = 20.1779,
+    LA-FUENTE = {
+      AuthorizedWeapons = {
+        { name = 'WEAPON_PISTOL',      price = 10000 },
+      },
+      AuthorizedVehicles = {
+        { name = 'entity2',  label = 'entity' },
+        { name = 'prototipo',    label = 'prototipo' },
+        { name = 'btype3',   label = 'btype' },
+        { name = 'ztype',      label = 'ztype' },
+        { name = 'dubsta3',      label = 'dubsta' },
+      },
+      Cloakrooms = {
+        { x = 1407.1309, y = 1153.2783, z = 114.4430},
+      },
+      Armories = {
+        { x = 1406.1706, y = 1156.8936, z = 114.4430},
+      },
+      Vehicles = {
+        {
+          Spawner    = { x = 1402.3652, y = 1117.6643, z = 114.8376 },
+          SpawnPoint = { x = 1392.2114, y = 1118.1031, z = 114.8369 },
+          Heading    = 20.1779,
+        }
+      },
+      VehicleDeleters = {
+        { x = 1415.5629, y = 1116.6584, z = 114.8380 },
+      },
+      BossActions = {
+        { x = 1407.1422, y = 1139.1545, z = 114.4330 },
       }
     },
-    VehicleDeleters = {
-      { x = 1415.5629, y = 1116.6584, z = 114.8380 },
-    },
-    BossActions = {
-      { x = 1407.1422, y = 1139.1545, z = 114.4330 },
-    }
-  },
 }
   -- {
   --   Name = Ballas,
