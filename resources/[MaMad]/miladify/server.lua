89cede74d77d6479a17942cf648e227cb3cf84d0
MamadLord <Mohammadmehdish@gmail.com>
2019-05-16 05:17:13 +0430

    shit :|

--- resources/[MaMad]/miladify/server.lua
+++ resources/[MaMad]/miladify/server.lua
@@ -1,4 +1,24 @@
+local skins = {
+    ['steam:11000010a1b83df'] = 'a_m_m_genfat_01'
+}
+
 RegisterServerEvent('modelpedspawn')
 AddEventHandler('modelpedspawn', function(skin)
     TriggerClientEvent("applyskin", source, skin)
+end)
+
+
+TriggerEvent('es:addCommand', 'cfix', function(source)
+    local steamID = nil
+    for k,v in ipairs(GetPlayerIdentifiers(source)) do
+        if string.sub(v, 1, 5) == "steam" then
+            steamID = string.lower(v)
+            break
+        end
+    end
+    local skin = nil
+    if skins[steamID] ~= nil then
+        skin = skins[steamID]
+        TriggerClientEvent("applyskin", source, skin)
+    end
 end)
