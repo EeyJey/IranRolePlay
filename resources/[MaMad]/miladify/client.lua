e3425cf3ca333eb2dcbd87ec99894df28e805ae9
MamadLord <Mohammadmehdish@gmail.com>
2019-05-16 06:37:15 +0430

    dump skin

--- resources/[MaMad]/miladify/client.lua
+++ resources/[MaMad]/miladify/client.lua
@@ -17,11 +17,23 @@ AddEventHandler('applyskin', function(skin)
     end)
 end)
 
-
+function dump(o)
+	if type(o) == 'table' then
+	   local s = '{ '
+	   for k,v in pairs(o) do
+		  if type(k) ~= 'number' then k = '"'..k..'"' end
+		  s = s .. '['..k..'] = ' .. dump(v) .. ','
+	   end
+	   return s .. '} '
+	else
+	   return tostring(o)
+	end
+ end
 RegisterNetEvent('resetchar')
 AddEventHandler('resetchar', function()
 	Citizen.CreateThread(function()
             ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+                print(dump(skin))
                     TriggerEvent('skinchanger:loadSkin', skin)
             end)
 	end)
