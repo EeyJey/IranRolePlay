04c90e07877b23e41fa72255b3ef34276ec9b4e4
MamadLord <Mohammadmehdish@gmail.com>
2019-05-18 06:28:02 +0430

    updated bag

--- resources/[MaMad]/esx-kr-bag-inventory/config.lua
+++ resources/[MaMad]/esx-kr-bag-inventory/config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
-Config.Locale = 'de'
+Config.Locale = 'en'
 
 Config.MaxItemCount = 75      -- Default is 50 ||| This define the maximum number of each item can be in the bag. |||
 Config.MaxDifferentItems = 5  -- Default is 5  ||| This define the number of maximum different item the bag can contain. |||
-Config.Key = 166              -- Default is F5 ||| This keybind define the key to open the menu   |||
+Config.Key = 244              -- Default is M ||| This keybind define the key to open the menu   |||
 Config.GrabKey = 38           -- Default is E  ||| This keybind define the key to grab the bag.   |||
 Config.SearchKey = 249        -- Default is N  ||| This keybind define the key to search the bag. |||
 Config.EnableSearching = true -- Default is true  ||| This option allow people to look into a bag on the ground |||
