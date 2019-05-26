200b80fc5b1707f129c363f1985f037f8532348f
MamadLord <Mohammadmehdish@gmail.com>
2019-05-26 08:26:04 +0430

    carcols added

--- resources/[MaMad]/ambu/__resource.lua
+++ resources/[MaMad]/ambu/__resource.lua
@@ -4,8 +4,10 @@ files {
     'vehicles.meta',
     'handling.meta',
     'carvariations.meta',
+    'carcols.meta',
 }
 
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'vehicle_variation_file' 'carvariations.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
