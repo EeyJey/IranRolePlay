717c0f0c1b10ba12ae2d833fad63111e9c0968f6
MamadLord <Mohammadmehdish@gmail.com>
2019-05-26 07:59:19 +0430

    carvaraddded

--- resources/[MaMad]/ambu/__resource.lua
+++ resources/[MaMad]/ambu/__resource.lua
@@ -3,7 +3,9 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 files {
     'vehicles.meta',
     'handling.meta',
+    'carvariations.meta',
 }
 
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_METADATA_FILE' 'carvariations.meta'
