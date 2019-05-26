2a0d60c017bc5e820b91cd9caac1ac7d7686efcc
MamadLord <Mohammadmehdish@gmail.com>
2019-05-26 11:54:51 +0430

    mcar added

---
+++ resources/[MaMad]/somefeatures/client.lua
@@ -0,0 +1,26 @@
+function SetVehicleMaxMods(vehicle)
+	local props = {
+		modEngine       =   3,
+		modBrakes       =   2,
+		windowTint      =   1,
+		modArmor        =   4,
+		modTransmission =   2,
+		modSuspension   =   -1,
+		modTurbo        =   true,
+	}
+
+	ESX.Game.SetVehicleProperties(vehicle, props)
+end
+
+
+RegisterNetEvent('sf:spawnVehicle')
+AddEventHandler('sf:spawnVehicle', function(model)
+	local playerPed = PlayerPedId()
+	local coords    = GetEntityCoords(playerPed)
+
+	ESX.Game.SpawnVehicle(model, coords, 90.0, function(vehicle)
+        TaskWarpPedIntoVehicle(playerPed,  vehicle, -1)
+		SetVehicleMaxMods(vehicle)
+        
+	end)
+end)
