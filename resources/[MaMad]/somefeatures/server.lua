38ebd406ff6460544487b4f7c4e3b8f7a741ef63
MamadLord <Mohammadmehdish@gmail.com>
2019-05-26 06:56:59 +0430

    time shit update

--- resources/[MaMad]/somefeatures/server.lua
+++ resources/[MaMad]/somefeatures/server.lua
@@ -1,15 +1,15 @@
 function GetTime()
 
 	local timestamp = os.time()
-	local d         = os.date('*t', timestamp).wday
+	local d         = os.date('%x', timestamp)
 	local h         = tonumber(os.date('%H', timestamp))
 	local m         = tonumber(os.date('%M', timestamp))
+	local s         = tonumber(os.date('%S', timestamp))
 
-	return {d = d, h = h, m = m}
-
+	return {d = d, h = h, m = m, s = s}
 end
 
 RegisterCommand('ti', function(source, args)
     local myTime = GetTime()
-	TriggerClientEvent('chatMessage', source, "[Time]: ", {253, 216, 53}, myTime.d..'  '..myTime.h..':'..myTime.m)
+	TriggerClientEvent('chatMessage', source, "[Time]: ", {253, 216, 53}, myTime.d..'  '..myTime.h..':'..myTime.m..':'..myTime.s)
 end)
