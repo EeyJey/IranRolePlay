31e917478b66e769eebea91ff41267656d5ef913
mamadlord <mohammadmehdish@gmail.com>
2019-05-31 12:58:35 +0430

    hoselam

--- resources/WeatherSnow/client.lua
+++ resources/WeatherSnow/client.lua
@@ -2,16 +2,16 @@ Citizen.CreateThread(function()
   while true do
     Citizen.Wait(1)
 
-	SetWeatherTypePersist("XMAS")
-	SetWeatherTypeNowPersist("XMAS")
-	SetWeatherTypeNow("XMAS")
-	SetOverrideWeather("XMAS")
-	SetForceVehicleTrails(true)
-	SetForcePedFootstepsTracks(true)
+	SetWeatherTypePersist("CLEAR")
+	SetWeatherTypeNowPersist("CLEAR")
+	SetWeatherTypeNow("CLEAR")
+	SetOverrideWeather("CLEAR")
+	--SetForceVehicleTrails(true)
+	--SetForcePedFootstepsTracks(true)
 
-	SetWind(1.0)
-	SetWindSpeed(11.99)
-	SetWindDirection(180.00)
+	--SetWind(1.0)
+	--SetWindSpeed(11.99)
+	--SetWindDirection(180.00)
 
   end
 end)
