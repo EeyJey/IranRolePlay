200e627bea4a0a5b7179137515d0a85324a665be
unknown <amir_hossein8731@yahoo.com>
2019-06-11 15:42:14 -0700

    go

--- resources/[aj]/car_radio/__resource.lua
+++ resources/[aj]/car_radio/__resource.lua
@@ -1,8 +1,8 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://37.59.47.192:8200/stream", volume = 0.2 }
-supersede_radio "RADIO_01_CLASS_ROCK" { url = "http://radio6o8.com:8000/stream", volume = 0.2 }
+supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://37.59.47.192:8200/stream", volume = 0.2, name = "Radio Javan" }
+supersede_radio "RADIO_01_CLASS_ROCK" { url = "http://radio6o8.com:8000/stream", volume = 0.2, name = "Radio Pir" }
 
 files {
 	"index.html"
