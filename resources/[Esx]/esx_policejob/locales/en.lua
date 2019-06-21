8f8d09d253f730c3807e61b6b2da7239c006c46d
unknown <amir_hossein8731@yahoo.com>
2019-06-21 04:54:05 -0700

    en

--- resources/[Esx]/esx_policejob/locales/en.lua
+++ resources/[Esx]/esx_policejob/locales/en.lua
@@ -1,10 +1,11 @@
 Locales['en'] = {
   -- Cloakroom
   ['cloakroom'] = 'locker room',
-  ['citizen_wear'] = 'civilian Outfit',
-  ['police_wear'] = 'police Outfit',
-  ['gilet_wear'] = 'orange reflective jacket',
-  ['bullet_wear'] = 'bulletproof vest',
+  ['citizen_wear'] = 'Civilian Outfit',
+  ['police_wear'] = 'Police Outfit',
+  ['swat_wear'] = 'Swat Outfit',
+  ['gilet_wear'] = 'Orange reflective jacket',
+  ['bullet_wear'] = 'Bulletproof vest',
   ['bullet_unwear'] = 'unwear bulletproof vest',
   ['no_outfit'] = 'there\'s no uniform that fits you!',
   ['open_cloackroom'] = 'press ~INPUT_CONTEXT~ to change ~y~clothes~s~.',
