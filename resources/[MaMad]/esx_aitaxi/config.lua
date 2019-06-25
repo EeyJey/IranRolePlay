12cafc76bf561c13a0e2dd0d0e8047e6d56bffb0
mamadlord <mohammadmehdish@gmail.com>
2019-06-25 21:25:31 +0430

    ai-taxi

---
+++ resources/[MaMad]/esx_aitaxi/config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+Config.hash = -573920724 -- hash of Taxidriver
+Config.vehicleHash = -956048545 -- hash of Taxi
+Config.Price = 200 --price per km
+Config.Speed = 20.0 -- Taxispeed (in player speed
+Config.DriveMode = 524863
+Config.SpawnBase = vector3(911.24, -177.84, 74.29)
+Config.SpawnPoints = {
+	DownTownCab = {x = 911.24, y = -177.84, z = 74.29, h = 240.32},
+	AirPort = {x = -731.76, y = -2575.79, z = 13.83, h = 332.92},
+	Ulsa = {x = -1612.60, y = 174.93, z = 59.80, h = 204.96},
+	SandyShores = {x = 1771.84, y = 3660.44, z = 34.37, h = 30.81},
+	SandyShores2 = {x = 2382.81, y = 5163.99, z = 49.12, h = 222.47},
+	PaletoBay = {x = -125.29, y = 6287.95, z = 31.38, h = 313.97},
+	MilitaryBase = {x = -2379.27, y = 3471.79, z = 24.46, h = 14.42},
+	ZancudoRiver = {x = -447.34, y = 2866.06, z = 35.98, h = 127.48},
+}
