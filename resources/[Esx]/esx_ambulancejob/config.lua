7f18d3d04d65f93404bc72c0b959bdecd3d4e05a
Matin_Dark <matin.m13581358@gmail.com>
2019-06-28 22:00:03 +0430

    Fix pol revive va time respawn ke ride bodam tosh

--- resources/[Esx]/esx_ambulancejob/config.lua
+++ resources/[Esx]/esx_ambulancejob/config.lua
@@ -4,7 +4,7 @@ Config.DrawDistance               = 100.0
 
 Config.Marker                     = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
 
-Config.ReviveReward               = 700  -- revive reward, set to 0 if you don't want it enabled
+Config.ReviveReward               = 4000  -- revive reward, set to 0 if you don't want it enabled
 Config.AntiCombatLog              = true -- enable anti-combat logging?
 Config.LoadIpl                    = true -- disable if you're using fivem-ipl or other IPL loaders
 
@@ -13,8 +13,8 @@ Config.Locale                     = 'en'
 local second = 1000
 local minute = 60 * second
 
-Config.EarlyRespawnTimer          = 2 * minute  -- Time til respawn is available
-Config.BleedoutTimer              = 10 * minute -- Time til the player bleeds out
+Config.EarlyRespawnTimer          = 5 * minute  -- Time til respawn is available
+Config.BleedoutTimer              = 60 * minute -- Time til the player bleeds out
 
 Config.EnablePlayerManagement     = false
 
