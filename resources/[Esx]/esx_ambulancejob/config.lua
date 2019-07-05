d33514d62972a7473c3d1dc226cfdd147e70ad2f
soundersquash <ahesari995@gmail.com>
2019-07-05 23:06:51 +0430

    medic

--- resources/[Esx]/esx_ambulancejob/config.lua
+++ resources/[Esx]/esx_ambulancejob/config.lua
@@ -4,7 +4,7 @@ Config.DrawDistance               = 100.0
 
 Config.Marker                     = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
 
-Config.ReviveReward               = 3500  -- revive reward, set to 0 if you don't want it enabled
+Config.ReviveReward               = 4000  -- revive reward, set to 0 if you don't want it enabled
 Config.AntiCombatLog              = true -- enable anti-combat logging?
 Config.LoadIpl                    = true -- disable if you're using fivem-ipl or other IPL loaders
 
@@ -13,13 +13,13 @@ Config.Locale                     = 'en'
 local second = 1000
 local minute = 60 * second
 
-Config.EarlyRespawnTimer          = 5 * minute  -- Time til respawn is available
-Config.BleedoutTimer              = 60 * minute -- Time til the player bleeds out
+Config.EarlyRespawnTimer          = 6 * minute  -- Time til respawn is available
+Config.BleedoutTimer              = 10 * minute -- Time til the player bleeds out
 
 Config.EnablePlayerManagement     = true
 
 Config.RemoveWeaponsAfterRPDeath  = true
-Config.RemoveCashAfterRPDeath     = true
+Config.RemoveCashAfterRPDeath     = false
 Config.RemoveItemsAfterRPDeath    = true
 
 -- Let the player pay for respawning early, only if he can afford it.
@@ -27,7 +27,6 @@ Config.EarlyRespawnFine           = false
 Config.EarlyRespawnFineAmount     = 5000
 
 Config.RespawnPoint = { coords = vector3(363.79, -578.72, 28.84), heading = 206.36 }
-Config.RespawnPointJailed = { coords = vector3(1799, 2483, -122), heading = 300}
 
 Config.Hospitals = {
 
@@ -164,37 +163,43 @@ Config.Hospitals = {
 Config.AuthorizedVehicles = {
 
 	ambulance = {
-		{ model = 'ambulance', label = 'Ambulance Van', price = 5000},
-		{ model = 'ambu1', label = 'Custom ambu2', price = 5000},
-		{ model = 'ambulance4', label = 'ambulance Benz', price = 5000}
+		{ model = 'ambulance', label = 'Ambulance Van', price = 1000},
+		{ model = 'ambu1', label = 'Custom ambu2', price = 1000},
+		{ model = 'ambulance4', label = 'ambulance Benz', price = 1000},
+		{ model = 'qrv', label = 'Ford', price = 3000}
 	},
 
 	doctor = {
-		{ model = 'ambulance', label = 'Ambulance Van', price = 4500},
-		{ model = 'ambu1', label = 'Custom ambu2', price = 4500},
-		{ model = 'ambulance4', label = 'ambulance Benz', price = 5000}
+		{ model = 'ambulance', label = 'Ambulance Van', price = 1000},
+		{ model = 'ambu1', label = 'Custom ambu2', price = 1000},
+		{ model = 'ambulance4', label = 'ambulance Benz', price = 1000},
+		{ model = 'qrv', label = 'Ford', price = 3000}
 	},
 
 	chief_doctor = {
-		{ model = 'ambulance', label = 'Ambulance Van', price = 3000},
-		{ model = 'ambu1', label = 'Custom ambu2', price = 3000},
-		{ model = 'ambulance4', label = 'ambulance Benz', price = 5000}
+		{ model = 'ambulance', label = 'Ambulance Van', price = 1000},
+		{ model = 'ambu1', label = 'Custom ambu2', price = 1000},
+		{ model = 'ambulance4', label = 'ambulance Benz', price = 1000},
+		{ model = 'qrv', label = 'Ford', price = 3000}
 	},
 	professor = {
-		{ model = 'ambulance', label = 'Ambulance Van', price = 3000},
-		{ model = 'ambu1', label = 'Custom ambu2', price = 3000},
-		{ model = 'ambulance4', label = 'ambulance Benz', price = 5000}
+		{ model = 'ambulance', label = 'Ambulance Van', price = 1000},
+		{ model = 'ambu1', label = 'Custom ambu2', price = 1000},
+		{ model = 'ambulance4', label = 'ambulance Benz', price = 1000},
+		{ model = 'qrv', label = 'Ford', price = 3000}
 	},
 	
 	pezeshk  = {
-		{ model = 'ambulance', label = 'Ambulance Van', price = 3000},
-		{ model = 'ambu1', label = 'Custom ambu2', price = 3000},
-		{ model = 'ambulance4', label = 'ambulance Benz', price = 5000}
+		{ model = 'ambulance', label = 'Ambulance Van', price = 1000},
+		{ model = 'ambu1', label = 'Custom ambu2', price = 1000},
+		{ model = 'ambulance4', label = 'ambulance Benz', price = 1000},
+		{ model = 'qrv', label = 'Ford', price = 3000}
 	},
 	boss = {
-		{ model = 'ambulance', label = 'Ambulance Van', price = 2000},
-		{ model = 'ambu1', label = 'Custom ambu2', price = 2000},
-		{ model = 'ambulance4', label = 'ambulance Benz', price = 5000}
+		{ model = 'ambulance', label = 'Ambulance Van', price = 1000},
+		{ model = 'ambu1', label = 'Custom ambu2', price = 1000},
+		{ model = 'ambulance4', label = 'ambulance Benz', price = 1000},
+		{ model = 'qrv', label = 'Ford', price = 3000}
 	}
 
 }
@@ -204,20 +209,20 @@ Config.AuthorizedHelicopters = {
 	ambulance = {},
 
 	doctor = {
-		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 150000 },
-		{ model = 'uh1mash', label = 'Medic Heli', price = 150000 }
+		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 10000 },
+		{ model = 'cgheli', label = 'Medic Heli', price = 20000 }
 	},
 
 	chief_doctor = {
-		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 150000 },
-		{ model = 'seasparrow', label = 'Sea Sparrow', price = 300000 },
-		{ model = 'uh1mash', label = 'Medic Heli', price = 150000 }
+		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 10000 },
+		{ model = 'seasparrow', label = 'Sea Sparrow', price = 30000 },
+		{ model = 'cgheli', label = 'Medic Heli', price = 20000 }
 	},
 
 	boss = {
 		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 10000 },
-		{ model = 'seasparrow', label = 'Sea Sparrow', price = 250000 },
-		{ model = 'uh1mash', label = 'Medic Heli', price = 150000 }
+		{ model = 'seasparrow', label = 'Sea Sparrow', price = 30000 },
+		{ model = 'cgheli', label = 'Medic Heli', price = 20000 }
 	}
 
 }
