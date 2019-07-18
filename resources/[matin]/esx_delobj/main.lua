687d61e3c9bb2ff832a2e6bf07d1ee5ba41208ce
Matin_Dark <matin.m13581358@gmail.com>
2019-07-18 09:09:43 +0430

    Fix delobject

--- resources/[matin]/esx_delobj/main.lua
+++ resources/[matin]/esx_delobj/main.lua
@@ -17,32 +17,33 @@ Citizen.CreateThread(function ()
 	PlayerData.job = job
   end)  
 
-  TriggerEvent('es:addCommand', 'delobject', function(source, args, rawCommand) 
-  
-    if PlayerData.job.name == "police" then
-      local prop = 0
-      local deelz = 10
-      local deelxy = 2
-      for offsety=-2,2 do
-          for offsetx=-2,2 do
-              for offsetz=-8,8 do
-                  local CoordFrom = GetEntityCoords(PlayerPedId(), true)
-                  local CoordTo = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, 0.0)
-                  local RayHandle = StartShapeTestRay(CoordFrom.x, CoordFrom.y, CoordFrom.z-(offsetz/deelz), CoordTo.x+(offsetx/deelxy), CoordTo.y+(offsety/deelxy), CoordTo.z-(offsetz/deelz), 16, PlayerPedId(), 0)
-                  local _, _, _, _, object = GetShapeTestResult(RayHandle)
-                  if object ~= 0 then
-                      prop = object
-                      break
-                  end
-              end
-          end
-      end
-      if prop == 0 then
-          TriggerEvent('chatMessage', '', {255,255,255}, '^8Error: ^0could not detect object.')
-      else
-          SetEntityAsMissionEntity(prop)
-          DeleteObject(prop)
-      end
+  RegisterCommand('delobject', function(source, args, rawCommand)
+  if PlayerData.job.name == "police" then
+    local prop = 0
+    local deelz = 10
+    local deelxy = 2
+    for offsety=-2,2 do
+        for offsetx=-2,2 do
+            for offsetz=-8,8 do
+                local CoordFrom = GetEntityCoords(PlayerPedId(), true)
+                local CoordTo = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, 0.0)
+                local RayHandle = StartShapeTestRay(CoordFrom.x, CoordFrom.y, CoordFrom.z-(offsetz/deelz), CoordTo.x+(offsetx/deelxy), CoordTo.y+(offsety/deelxy), CoordTo.z-(offsetz/deelz), 16, PlayerPedId(), 0)
+                local _, _, _, _, object = GetShapeTestResult(RayHandle)
+                if object ~= 0 then
+                    prop = object
+                    break
+                end
+            end
+        end
     end
+    if prop == 0 then
+        TriggerEvent('chatMessage', '', {255,255,255}, '^8Error: ^0could not detect object.')
+    else
+        SetEntityAsMissionEntity(prop)
+        DeleteObject(prop)
+	 end
+    else
+	ESX.ShowNotification('~r~~h~Shoma police nistid')
+	end
   
-  end, {help here})
+  end, false)
