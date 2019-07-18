837265fc6098a40fe1a27a37b39ed48e6f4f8fc8
Matin_Dark <matin.m13581358@gmail.com>
2019-07-18 08:28:12 +0430

    Change register command delobject

--- resources/[matin]/esx_delobj/main.lua
+++ resources/[matin]/esx_delobj/main.lua
@@ -17,30 +17,32 @@ Citizen.CreateThread(function ()
 	PlayerData.job = job
   end)  
 
-  RegisterCommand('delobject', function(source, args, rawCommand)
-  if PlayerData.job.name == "police" then
-    local prop = 0
-    local deelz = 10
-    local deelxy = 2
-    for offsety=-2,2 do
-        for offsetx=-2,2 do
-            for offsetz=-8,8 do
-                local CoordFrom = GetEntityCoords(PlayerPedId(), true)
-                local CoordTo = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, 0.0)
-                local RayHandle = StartShapeTestRay(CoordFrom.x, CoordFrom.y, CoordFrom.z-(offsetz/deelz), CoordTo.x+(offsetx/deelxy), CoordTo.y+(offsety/deelxy), CoordTo.z-(offsetz/deelz), 16, PlayerPedId(), 0)
-                local _, _, _, _, object = GetShapeTestResult(RayHandle)
-                if object ~= 0 then
-                    prop = object
-                    break
-                end
-            end
-        end
+  TriggerEvent('es:addCommand', 'delobject', function(source, args, rawCommand) 
+  
+    if PlayerData.job.name == "police" then
+      local prop = 0
+      local deelz = 10
+      local deelxy = 2
+      for offsety=-2,2 do
+          for offsetx=-2,2 do
+              for offsetz=-8,8 do
+                  local CoordFrom = GetEntityCoords(PlayerPedId(), true)
+                  local CoordTo = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, 0.0)
+                  local RayHandle = StartShapeTestRay(CoordFrom.x, CoordFrom.y, CoordFrom.z-(offsetz/deelz), CoordTo.x+(offsetx/deelxy), CoordTo.y+(offsety/deelxy), CoordTo.z-(offsetz/deelz), 16, PlayerPedId(), 0)
+                  local _, _, _, _, object = GetShapeTestResult(RayHandle)
+                  if object ~= 0 then
+                      prop = object
+                      break
+                  end
+              end
+          end
+      end
+      if prop == 0 then
+          TriggerEvent('chatMessage', '', {255,255,255}, '^8Error: ^0could not detect object.')
+      else
+          SetEntityAsMissionEntity(prop)
+          DeleteObject(prop)
+      end
     end
-    if prop == 0 then
-        TriggerEvent('chatMessage', '', {255,255,255}, '^8Error: ^0could not detect object.')
-    else
-        SetEntityAsMissionEntity(prop)
-        DeleteObject(prop)
-    end
-  end
-end, false)
+  
+  end, {help here})
